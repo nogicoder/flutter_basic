27812567d7d515a8bea44dd13f359b07ec59b6da
MAI <B.MAI@bycn.bouygues-construction.com>
2019-06-12 18:16:38 +0700

    Adding 2nd screen Flutter Tut 2

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,19 @@ import "package:english_words/english_words.dart";
 
 void main() => runApp(MyApp());
 
+final _collections = <WordPair>[];
+final _favourites = Set<WordPair>();
+
 class MyApp extends StatelessWidget {
   @override
   build(context) {
     return MaterialApp(
-      title: "Name Generator",
-      theme: ThemeData(
-        primaryColor: Colors.deepOrange,
-        scaffoldBackgroundColor: Colors.yellow[100],
-      ),
-      home: WordGen()
-    );
+        title: "Name Generator",
+        theme: ThemeData(
+          primaryColor: Color(0xFF142d4c),
+          scaffoldBackgroundColor: Color(0xFFececec),
+        ),
+        home: WordGen());
   }
 }
 
@@ -21,37 +23,37 @@ class WordGen extends StatefulWidget {
   createState() => WordGenState();
 }
 
+class SecondScreen extends StatelessWidget {
+
+
+  final tiles = _favourites.map((WordPair pair) {
+    return ListTile(
+        title: Text(pair.asPascalCase,
+            style: TextStyle(fontSize: 18, color: Colors.black45)
+        )
+      );
+  });
+
+  final divided = ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+  @override
+  build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Favourites"),
+        leading: IconButton(icon: Icon(Icons.all_inclusive), onPressed: () {Navigator.pop(context);})
+      ),
+      body: ListView(children: divided)
+    );
+  }
+}
+
 class WordGenState extends State<WordGen> {
-  void _displayFavourites() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          
-          final tiles = _favourites.map(
-            (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: TextStyle(fontSize: 18, color: Colors.red)
-                )
-              );
-            }
-          );
-
-          final divided = ListTile
-          .divideTiles(
-            context: context,
-            tiles: tiles
-          )
-          .toList();
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text("Favourites"),
-            ),
-            body: ListView(children: divided)
-          );
-        } 
+  
+  void _changeScreen() {
+    Navigator.of(context)
+      .push(MaterialPageRoute(
+        builder: (context) => SecondScreen()
       )
     );
   }
@@ -59,59 +61,46 @@ class WordGenState extends State<WordGen> {
   @override
   build(context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Name Generator"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.list),
-            onPressed: _displayFavourites
-          )
-        ],
-      ),
-      body: _createWordList()
-    );
+        appBar: AppBar(
+          title: Text("Name Generator"),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.list), onPressed: _changeScreen)
+          ],
+        ),
+        body: _createWordList());
   }
-
-  final _collections = <WordPair>[];
-  final _favourites = Set<WordPair>();
 
   _generateTile(WordPair pair) {
     final favoured = _favourites.contains(pair);
     return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: TextStyle(fontSize: 18, color: Colors.red)
-      ),
-      trailing: Icon(
-        favoured ? Icons.bookmark : Icons.bookmark_border,
-        color: favoured ? Colors.red : Colors.blueAccent
-      ),
-      onTap: () {
-        setState(() {
-          if (favoured) {
-            _favourites.remove(pair);
-          } else {
-            _favourites.add(pair);
-          }
+        title: Text(pair.asPascalCase,
+            style: TextStyle(fontSize: 18, color: Colors.black45)),
+        trailing: Icon(favoured ? Icons.bookmark : Icons.bookmark_border,
+            color: favoured ? Colors.red : Colors.blueAccent),
+        onTap: () {
+          setState(() {
+            if (favoured) {
+              _favourites.remove(pair);
+            } else {
+              _favourites.add(pair);
+            }
+          });
         });
-      }
-    );
   }
 
   _createWordList() {
     return ListView.builder(
-      padding: EdgeInsets.all(20),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return Divider();
+        padding: EdgeInsets.all(20),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
 
-        final index = i ~/ 2;
-        
-        if (index >= _collections.length) {
-          _collections.addAll(generateWordPairs().take(10));
-        }
-        
-        return _generateTile(_collections[index]);
-      }
-    );
+          final index = i ~/ 2;
+
+          if (index >= _collections.length) {
+            _collections.addAll(generateWordPairs().take(10));
+          }
+
+          return _generateTile(_collections[index]);
+        });
   }
 }
